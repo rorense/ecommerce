4bcfe5404bb20846bce8c3086b403827a3795f91
rorense <rkorense@gmail.com>
2023-08-03 20:53:33 +1200

    Footer component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import "./Footer.css"
 
+// The footer component
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <div className="top">
+            <div className="item">
+                <h1>Categories</h1>
+                <span>Women</span>
+                <span>Men</span>
+                <span>Shoes</span>
+                <span>Accessories</span>
+                <span>New Arrivals</span>
+            </div>
+            <div className="item">
+                <h1>Links</h1>
+                <span>FAQ</span>
+                <span>Pages</span>
+                <span>Stores</span>
+                <span>Compare</span>
+                <span>Cookies</span>
+            </div>
+            <div className="item">
+                <h1>About</h1>
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span>
+            </div>
+            <div className="item">
+                <h1>Contact</h1>
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span>
+            </div>
+        </div>
+        <div className="bottom">
+            <div className="left">
+                <span className="logo">Ryan Store</span>
+                <span className="copyright">© Copyright 2023. All Rights Reserved</span>
+            </div>
+            <div className="right">
+                <img src="/img/payment.png" alt=""></img>
+            </div>
+        </div>
+    </div>
   )
 }
 
