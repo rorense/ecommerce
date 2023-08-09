fe88d6df3d009126e78f97e03c9089145c03e343
rorense <rkorense@gmail.com>
2023-08-09 22:03:40 +1200

    Working on category cards

---
+++ src/Components/Categories/Category.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./Category.css"
+
+function Category() {
+  return (
+    <div className='categories'>
+        <div className="col">
+            <div className="row"></div>
+            <div className="row"></div>
+        </div>
+        <div className="col">
+            <div className="row"></div>
+        </div>
+        <div className="col-l">
+            <div className="row">
+                <div className="col"></div>
+                <div className="col"></div>
+            </div>
+            <div className="row"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Category
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Slider from '../../Components/Slider/Slider';
 import "./Home.css";
 import FeaturedProduct from '../../Components/FeaturedProduct/FeaturedProduct';
+import Category from '../../Components/Categories/Category';
 
 function Home() {
   return (
     <div className='home'>
       <Slider />
       <FeaturedProduct type="Featured" />
+      <Category />
       <FeaturedProduct type="Trending" />
     </div>
   )
