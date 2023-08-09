3ce449691243486673f5417e09e74034de9f8b4e
rorense <rkorense@gmail.com>
2023-08-09 14:38:33 +1200

    Finished working on card components

--- src/Components/FeaturedProduct/FeaturedProduct.js
+++ src/Components/FeaturedProduct/FeaturedProduct.js
@@ -4,12 +4,13 @@ import Card from '../Card/Card';
 
 function FeaturedProduct({ type }) {
 
+    // Product data as an array input
     const data = [
         {
             id: 1,
             img: "https://images.pexels.com/photos/5730956/pexels-photo-5730956.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            img2: "",
-            title: "",
+            img2: "https://images.pexels.com/photos/5214139/pexels-photo-5214139.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            title: "Shoes",
             isNew: true,
             oldPrice: 19,
             price: 12,
@@ -18,7 +19,7 @@ function FeaturedProduct({ type }) {
             id: 2,
             img: "https://images.pexels.com/photos/1943846/pexels-photo-1943846.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
             img2: "",
-            title: "",
+            title: "Dress",
             isNew: true,
             oldPrice: 34,
             price: 26,
@@ -27,11 +28,20 @@ function FeaturedProduct({ type }) {
             id: 3,
             img: "https://images.pexels.com/photos/2850487/pexels-photo-2850487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
             img2: "",
-            title: "",
+            title: "Shirt",
             isNew: false,
             oldPrice: 19,
             price: 12,
-        }
+        },
+        {
+            id: 4,
+            img: "https://images.pexels.com/photos/160438/ankle-socks-children-socks-knitted-greeting-160438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            img2: "",
+            title: "Socks",
+            isNew: false,
+            oldPrice: 19,
+            price: 12,
+        },
     ]
 
   return (
@@ -43,6 +53,7 @@ function FeaturedProduct({ type }) {
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
         </div>
         <div className="bottom">
+            {/* Mapping through each item and displaying */}
             { data.map(item =>(
                 <Card item={ item } key={ item.id } />
             ) )}
