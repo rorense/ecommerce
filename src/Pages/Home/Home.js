d8bbfa869db43505c1761d27e169ad797efe08f7
rorense <rkorense@gmail.com>
2023-08-10 21:36:47 +1200

    Working on contact page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import Slider from '../../Components/Slider/Slider';
 import "./Home.css";
 import FeaturedProduct from '../../Components/FeaturedProduct/FeaturedProduct';
 import Category from '../../Components/Categories/Category';
+import Contact from '../../Components/Contact/Contact';
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <FeaturedProduct type="Featured" />
       <Category />
       <FeaturedProduct type="Trending" />
+      <Contact />
     </div>
   )
 }
